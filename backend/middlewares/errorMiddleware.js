31b64b2352fb96204ea5d166dcc1a5d2db44fa4e
Digvijay7310 <kumardigvijay377@gmail.com>
2025-11-17 14:21:38 +0530

    delete all directory

--- backend/middlewares/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode).json({
-        message: err.message || "Internal server Error",
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-}
--- backend/middlewares/validateRequest.js
+++
@@ -1,9 +0,0 @@
-import { body } from "express-validator";
-
-export const validateRequest = (req, res, next) => {
-    const errors = validateRequest(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
-    }
-    next()
-}
