969b6c9a4bb1f74fa91fd25baf86679a6c2f6f2b
= <=>
2026-04-02 11:03:45 +0530

    Delete both folders due to bugs

--- frontend/src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Logo() {
-  return (
-    <div className='bg-red-600 rounded-bl-2xl rounded-tl-xl text-white font-bold  text-lg px-3 py-1'>Ve<span className='text-yellow-300'>s</span>sy</div>
-  )
-}
-
-export default Logo
